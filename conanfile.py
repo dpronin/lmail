0ae786b22c49da04b238e59d4fcc2f61cecd4350
Denis Pronin <dannftk@yandex.ru>
2021-08-02 18:26:32 +0300

    version 1.0.0.

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class LMail(ConanFile):
     name = "lmail"
-    version = "0.4.1"
+    version = "1.0.0"
     author = "Denis Pronin"
     url = "https://github.com/dannftk/lmail"
     description = "A tool for mailing in a local host"
