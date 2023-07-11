ad1feb9d876fc052d7b74b9a1d7e24ff47f370b9
Denis Pronin <dannftk@yandex.ru>
2023-07-12 00:33:18 +0300

    src/cli.cpp refactoring
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- src/cli.cpp
+++ src/cli.cpp
@@ -32,7 +32,7 @@ void Cli::run()
     for (user_input_t user_input; !fsm.is(X) && ctx(user_input);) {
         args_t args;
         std::istringstream iss{user_input};
-        std::ranges::remove_copy_if(std::views::istream<arg_t>(iss), std::back_inserter(args), [](auto const& arg) { return arg.empty(); });
+        std::ranges::remove_copy_if(std::views::istream<arg_t>(iss), std::back_inserter(args), &arg_t::empty);
         if (auto cmd = ctx.parse(std::move(args)))
             cmd->exec();
     }
